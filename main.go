a134ac237df4dc7f8a764c507f0854004f80a74f
Jakub Gabrys <jakub@volt.io>
2024-12-14 22:49:26 +0100

    config: allow stage domain cors access

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 func setupRouter(container *container.Container) *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5000"}, // Add your domain and localhost
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5000", "https://pyrhouse-frontend-p2sbw.ondigitalocean.app"}, // Add your domain and localhost
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
