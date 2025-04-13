24575c06523179bbe7f06ee8acdfee214e95457b
Jakub Gabrys <jakub@volt.io>
2025-04-13 02:57:46 +0200

    attempt to fix PG connections

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -12,6 +13,12 @@ func NewPostgresConnection(dbURL string) (*sql.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to postgres: %w", err)
 	}
+
+	// Konfiguracja puli połączeń
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxIdleTime(1 * time.Minute)
 
 	if err := db.Ping(); err != nil {
 		return nil, fmt.Errorf("could not ping the database: %w", err)
