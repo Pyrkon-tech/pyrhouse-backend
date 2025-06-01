c8e26ca0db5cc24d7f1e075d6ab64d3ba04b1ac9
Jakub Gabrys <jakub@volt.io>
2025-06-01 22:56:03 +0200

    chore: allow more origin

--- pkg/metadata/origin.go
+++ pkg/metadata/origin.go
@@ -10,14 +10,17 @@ type Origin string
 const (
 	OriginDrugaEra Origin = "druga-era"
 	OriginProbis   Origin = "probis"
+	OriginNetland  Origin = "netland"
 	OriginTargi    Origin = "targowe"
+	OriginDjSound  Origin = "dj-sound"
+	OriginOkiEvent Origin = "oki-event"
 	OriginPersonal Origin = "personal"
 	OriginOther    Origin = "other"
 )
 
 func (o Origin) IsValid() bool {
 	switch o {
-	case OriginDrugaEra, OriginProbis, OriginTargi:
+	case OriginDrugaEra, OriginProbis, OriginTargi, OriginNetland, OriginDjSound, OriginOkiEvent:
 		return true
 	default:
 		return false
@@ -38,8 +41,8 @@ func NewOrigin(value string) (Origin, error) {
 	origin := Origin(normalized)
 	if !origin.IsValid() && !origin.isPredefined() {
 		return origin, fmt.Errorf(
-			"value not valid, only valid values are: %s, %s, %s, %s, %s",
-			OriginDrugaEra, OriginProbis, OriginTargi, OriginPersonal, OriginOther,
+			"value not valid, only valid values are: %s, %s, %s, %s, %s, %s, %s, %s",
+			OriginDrugaEra, OriginProbis, OriginTargi, OriginNetland, OriginDjSound, OriginOkiEvent, OriginPersonal, OriginOther,
 		)
 	}
 
