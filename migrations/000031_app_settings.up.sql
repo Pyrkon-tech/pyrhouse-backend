d54084a1f9061a4acec7aa9e94dde80cfca9a5ab
Jakub Gabrys <jakub@volt.io>
2026-02-24 00:19:07 +0100

    fix: remove insert id

--- migrations/000031_app_settings.up.sql
+++ migrations/000031_app_settings.up.sql
@@ -4,7 +4,3 @@ CREATE TABLE app_settings (
     description VARCHAR(512),
     updated_at TIMESTAMP DEFAULT NOW()
 );
-
-INSERT INTO app_settings (key, value, description) VALUES
-  ('equipment_request.sheet_id', '16BytrbWmyWeBGnlSIDZn1Lnb5rdspoQu_rpc5m5Vtbc', 'Google Sheets document ID for equipment requests'),
-  ('equipment_request.sheet_name', 'Zamówienia', 'Sheet tab name within the document');
